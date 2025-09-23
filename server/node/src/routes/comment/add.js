e39695278765ba6047d5750b76ebe2e09d234d0a
shbaek <>
2025-09-23 12:08:06 +0900

    indent 4 -> 2 수정

--- server/node/src/routes/comment/add.js
+++ server/node/src/routes/comment/add.js
@@ -5,40 +5,40 @@ const userDB = require('../../db/userDB')
 
 
 router.post('/addComment', (req, res) => {
-    console.log('/comment/addComment >> ' + JSON.stringify(req.body));
+	console.log('/comment/addComment >> ' + JSON.stringify(req.body));
 
-    let { photoKey, ownerKey, comment } = req.body;
-    console.log('++++' + photoKey + ' / ' + comment);
+	let { photoKey, ownerKey, comment } = req.body;
+	console.log('++++' + photoKey + ' / ' + comment);
 
-    (async () => {
-        try {   
-            console.log('>>>>>>>>>>');
-            let commentKey = await commentDB.addComment(ownerKey, comment, photoKey);
-            console.log('comment key' + commentKey);
-            if (commentKey < 1) {
-                res.json({
-                    result: 0,
-                    msg: '댓글을 추가하지 못했습니다',
-                    data: null
-                })
-                return
-            }
+	(async () => {
+		try {   
+			console.log('>>>>>>>>>>');
+			let commentKey = await commentDB.addComment(ownerKey, comment, photoKey);
+			console.log('comment key' + commentKey);
+			if (commentKey < 1) {
+					res.json({
+							result: 0,
+							msg: '댓글을 추가하지 못했습니다',
+							data: null
+					})
+					return
+			}
 
-            res.json({
-                result: 1,
-                msg: '',
-                data: {
-                    commentKey: commentKey
-                }
-            })
-        } catch (error) {
-            res.json({
-                    result: 0,
-                    msg: error.message,
-                    data: null
-            })
-        }
-    })();
+			res.json({
+					result: 1,
+					msg: '',
+					data: {
+							commentKey: commentKey
+					}
+			})
+		} catch (error) {
+			res.json({
+				result: 0,
+				msg: error.message,
+				data: null
+			})
+		}
+	})();
 
 });
 
